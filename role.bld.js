94ac18ff182771cba5e416df0c33bb19acaf19df
Daniel Chirita <dschirita@gmail.com>
2019-03-15 03:15:32 +0400

    function renaming and cleaning deprecated code

--- role.bld.js
+++ role.bld.js
@@ -22,53 +22,6 @@ var builder = {
         }
         else if(utils.collect(creep)){}
     },
-    
-    
-    
-    
-    
-    // // #################################################################################
-    // gobuild: function (creep){
-    //     var targets = vars.home.find(FIND_CONSTRUCTION_SITES);
-    //     if(targets.length>0) {
-    //         if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-    //             creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-    //         }    
-    //     }
-    //     return targets.length > 0
-    // },
-    // // #################################################################################
-    maintain: function (creep){
-        var targets = vars.home.find(
-            FIND_STRUCTURES, {
-                filter: (structure) => {return (structure.structureType == STRUCTURE_TOWER)
-                && structure.energy < structure.energyCapacity;}
-            }
-        );
-        if(targets.length > 0) {
-            if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-            }
-        }
-        return targets.length > 0
-    },
-    // #################################################################################
-    // gorepair: function (creep){
-    //     console.log("repairing")
-    //     var targets = vars.home.find(
-    //         FIND_STRUCTURES, {
-    //             filter: (structure) => {return (structure.structureType == STRUCTURE_ROAD || structure.structureType == STRUCTURE_TOWER || structure.structureType == STRUCTURE_CONTAINER)
-    //             && structure.hits < structure.hitsMax;}
-    //         }
-    //     );
-    //     if(targets.length > 0) {
-    //         if(creep.repair(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-    //             creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-    //         }
-    //     }
-    //     return targets.length > 0
-    // }
-    
 };
 
 module.exports = builder;
--- role.harv.js
+++ role.harv.js
@@ -12,48 +12,8 @@ var harvester = {
             else if (utils.collect(creep)){}
         }
         else if(utils.maintain(creep)){}
-        else if(utils.upg_controller(creep)){}
-    },
-    
-    // // ############################################################################
-    // pickup_dead: function(creep){
-    //     var sources = creep.room.find(FIND_TOMBSTONES,{
-    //         filter: (tomb) => {return (tomb.store["RESOURCE_ENERGY"] > 0)}
-    //     });
-    //     if(creep.withdraw(sources[0],RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-    //         creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-    //     }
-    //     return sources.length > 0
-    // },
-    // collect: function(creep){
-    //     var sources = creep.room.find(FIND_SOURCES);
-    //     if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
-    //         creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
-    //     }
-    //     return sources.length > 0
-    // },
-    maintain: function(creep){
-        var targets = Game.rooms["E7N17"].find(FIND_MY_STRUCTURES, {
-            filter: (structure) => {
-                return (structure.structureType == STRUCTURE_EXTENSION 
-                    || structure.structureType == STRUCTURE_CONTAINER 
-                    // || structure.structureType == STRUCTURE_TOWER 
-                    ) && structure.energy < structure.energyCapacity;
-            }
-        });
-        if(targets.length > 0) {
-            if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-            }
-        }
-        return targets.length > 0
-    },
-    // upg_controller: function(creep){
-    //     if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-    //         creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#ffffff'}});
-    //     }
-    // }
-    
+        else if(utils.upg_home_controller(creep)){}
+    },    
 };
 
 module.exports = harvester;
